ef0a214b9821b8400ec8c612b273d1005e58746a
BelKed <66956532+BelKed@users.noreply.github.com>
2022-12-07 00:59:22 +0100

    Change the design a bit

--- pyrotechnik.py
+++ pyrotechnik.py
@@ -1,18 +1,19 @@
 import random
 import tkinter
+from PIL import Image, ImageTk
 
 
 # -----------------------------
 # |  Constants and variables  |
 # -----------------------------
 
-canvas_width, canvas_height = 800, 400
+canvas_width = 800
 colors = ("red", "green", "blue", "yellow", "black", "violet", "gray", "pink")
 red, green, blue, gray = "#e36666", "#76e2af", "#32b1d3", "#282c34"
 
-time_width = 200
-cable_width, cable_height = canvas_width - (time_width + 50), 20
-padding_bottom = 2
+cable_width = canvas_width - 50
+cable_height = 30
+padding = 4
 
 time_element = 0
 update_time_pointer = 0
@@ -20,6 +21,8 @@ time = 15 + 1
 
 lives_element = 0
 lives = 3 + 1
+
+images = []
 
 
 # -----------------------------
@@ -31,21 +34,14 @@ def update_time():
     global time_element, time, update_time_pointer
 
     time -= 1
+
+    canvas.delete(time_element)
+    time_element = canvas.create_text(75, 60, text=time, fill=red, font="Arial 70 bold")
+
+    update_time_pointer = canvas.after(1000, update_time)
+
     if time <= 0:
-        loss()
-    else:
-        canvas.delete(time_element)
-
-        time_element = canvas.create_text(
-            canvas_width - 50,
-            canvas_height / 2 + 50,
-            text=time,
-            fill=red,
-            font="Arial 70 bold",
-            anchor="e",
-        )
-
-        update_time_pointer = canvas.after(1000, update_time)
+        end_screen("loss")
 
 
 # -----------------------------
@@ -55,48 +51,36 @@ def update_lives():
     global lives_element, lives
 
     lives -= 1
+
+    canvas.delete(lives_element)
+    lives_element = canvas.create_text(
+        canvas_width - 40, 60, text="❤" * lives, fill=red, font="Arial 70 bold", anchor="e"
+    )
+
     if lives <= 0:
-        loss()
+        end_screen("loss")
 
 
 # -----------------------------
 
 
-def cancel_events():
+def end_screen(status):
     global update_time_pointer
 
     canvas.after_cancel(update_time_pointer)
     canvas.unbind_all("<Button-1>")
 
+    image = Image.new("RGBA", (canvas_width, canvas_height), (0, 0, 0, 160))
+    images.append(ImageTk.PhotoImage(image))
+    canvas.create_image(0, 0, image=images[-1], anchor="nw")
+    canvas.create_rectangle(0, 0, canvas_width, canvas_height, width=0)
 
-# -----------------------------
-
-
-def loss():
-    cancel_events()
-    canvas.delete("all")
-
-    canvas.create_text(
-        canvas_width / 2,
-        canvas_height / 2,
-        text="BOOOOOOOM",
-        fill=red,
-        font="Arial 50 bold",
-    )
-
-
-# -----------------------------
-
-
-def win():
-    cancel_events()
+    text, fill = "Prehral si", red
+    if status == "win":
+        text, fill = "Vyhral si", green
 
     canvas.create_text(
-        canvas_width / 2,
-        canvas_height - 50,
-        text="Vyhral si",
-        fill=green,
-        font="Arial 30 bold",
+        canvas_width / 2, canvas_height / 2, text=text, fill=fill, font="Arial 50 bold"
     )
 
 
@@ -107,16 +91,15 @@ def clicked(event):
     x = event.x
     y = event.y
 
-    if 50 <= x <= cable_width + 50:
-        cable_y = (canvas_height - (cables_count - 1) * cable_height) / 2 + 50
-
+    if 25 <= x <= cable_width + 25:
+        cable_y = 150
         for i in range(cables_count):
-            if cable_y <= y <= cable_y + cable_height - padding_bottom:
+            if cable_y <= y <= cable_y + cable_height:
                 break
             cable_y += cable_height
 
         if i == correct_cable:
-            win()
+            end_screen("win")
         else:
             update_lives()
 
@@ -133,28 +116,26 @@ while True:
         break
 
 
+canvas_height = cables_count * cable_height + 175
+
 canvas = tkinter.Canvas(width=canvas_width, height=canvas_height, bg=gray)
 canvas.pack()
 
 canvas.create_text(canvas_width / 2, 45, text="PYROTECHNIK", fill=blue, font="Arial 30 bold")
 canvas.create_text(canvas_width / 2, 75, text="Označ správny káblik", fill=green, font="Arial 20")
 
-
-x = 50 + cable_width / 2
-y = (canvas_height - cables_count * cable_height) / 2 + 50
-
+x = 25
+y = 150
 for i in range(cables_count):
     canvas.create_rectangle(
-        x - cable_width / 2,
-        y - cable_height / 2 + (i + 1) * cable_height,
-        x + cable_width / 2,
-        y + cable_height / 2 + (i + 1) * cable_height - padding_bottom,
-        fill=colors[i],
-        width=0,
+        x, y, x + cable_width, y + cable_height, fill=colors[i], outline=gray, width=padding
     )
+
+    y += cable_height
 
 
 correct_cable = random.randrange(cables_count)
+print("Correct cable:", correct_cable)
 
 canvas.bind_all("<Button-1>", clicked)
 
